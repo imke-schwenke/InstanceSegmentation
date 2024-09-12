8e65777829f19dcd632a36b7f42082e53e1ab424
imke-schwenke <ischwenk@smail.uni-koeln.de>
2024-09-12 22:03:48 +0200

    deleted not needed files

--- CElegansConfig.py
+++
@@ -1,9 +0,0 @@
-from mrcnn.config import Config
-
-class CElegansConfig(Config):
-    NAME = "c_elegans"
-    GPU_COUNT = 1
-    IMAGES_PER_GPU = 1
-    NUM_CLASSES = 2 # Background + worm
-    STEPS_PER_EPOCH = 100
-    DETECTION_MIN_CONFIDENCE = 0.9
